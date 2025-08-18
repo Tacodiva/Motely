4a4358355d7b93fd557a430362b13c9f5ecf5f87
Nathanial P. Howard <admin@optimuspi.com>
2025-08-17 19:25:27 -0500

    these might be duplicates, they were def. needed for my json configs is why I added

---
+++ enums/MotelyFilterItemType.cs
@@ -0,0 +1,18 @@
+namespace Motely;
+
+/// <summary>
+/// Types of items that can be filtered in searches
+/// </summary>
+public enum MotelyFilterItemType
+{
+    Joker,
+    SoulJoker,
+    TarotCard,
+    PlanetCard,
+    SpectralCard,
+    SmallBlindTag,
+    BigBlindTag,
+    Voucher,
+    PlayingCard,
+    Boss
+}
---
+++ enums/MotelyJokerSticker.cs
@@ -0,0 +1,10 @@
+namespace Motely
+{
+    public enum MotelyJokerSticker
+    {
+        None = 0,
+        Eternal = 1,
+        Perishable = 2,
+        Rental = 3
+    }
+}
